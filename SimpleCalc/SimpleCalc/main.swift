4af9da9b1941008ead3bc2e04e35dc8fa0b82a61
creiland <creiland19@gmail.com>
2019-01-13 21:00:07 -0800

    finished code, need to test

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,44 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        var ret : Int = 0
+        if args.count == 3 {
+            let first = Int(args[0])
+            let operation = args[1]
+            let second = Int(args[2])
+            if operation == "+" {
+                ret = first! + second!
+            } else if operation == "-" {
+                ret = first! - second!
+            } else if operation == "/" {
+                ret = first! / second!
+            } else if operation == "*" {
+                ret = first! * second!
+            } else if operation == "%" {
+                ret = first! % second!
+            }
+        } else {
+            let operation = args[args.count]
+            if operation == "count" {
+                ret = args.count - 1
+            } else if operation == "avg" {
+                ret = 0
+                for _ in 0...(args.count - 1){
+                    ret += 1
+                }
+            } else if operation == "fact" {
+                let num = Int(args[0])
+                if num! > 0 {
+                    ret = 1
+                    for i in 1...num! {
+                        ret = ret * i
+                    }
+                } else {
+                    ret = 1
+                }
+            }
+        }
+        return ret
     }
     
     public func calculate(_ arg: String) -> Int {
@@ -23,4 +61,3 @@ let first = readLine()!
 let operation = readLine()!
 let second = readLine()!
 print(Calculator().calculate([first, operation, second]))
-
